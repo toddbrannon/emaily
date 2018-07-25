9a681a977f34b3f7ad7ba1f1ab2c93a8e2767bfc
Todd Brannon <toddqbrannon@gmail.com>
2018-07-25 05:49:21 -0500

    complete google auth

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -12,15 +23,40 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-            then((existingUser) => {
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
                 if (existingUser) {
                     //we already have a record with that given profile ID
+                    done(null, existingUser);
                 } else {
                     // we don't already have a record with that given profile ID
-                    new User({ googleId: profile.id }).save();
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
                 }
-            })
+            });
+
+        }
+    )
+);
+
+passport.use(
+    new FacebookStrategy({
+            clientID: keys.facebookClientID,
+            clientSecret: keys.facebookClientSecret;
+            callbackURL: '/auth/facebook/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ facebookId: profile.id }).then((existingUser) => {
+                if (existingUser) {
+                    //we already have a record with that given profile ID
+                    done(null, existingUser);
+                } else {
+                    // we don't already have a record with that given profile ID
+                    new User({ facebookId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
 
         }
     )
